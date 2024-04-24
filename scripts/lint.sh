e60cf48f16193417149bd3dec5a0ec2208723827
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-04-24 10:13:05 -0500

    Create lint.sh

---
+++ scripts/lint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+source .venv/bin/activate
+
+black app --line-length 120 --exclude migrations/ --exclude fixtures/
+
+flake8 app --max-line-length 120 --extend-exclude .idea/,.venv/,migrations/
+
+isort app --profile black
