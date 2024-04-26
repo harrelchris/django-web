577e654ccfd8667aa594e78824487c1d97d31df1
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-04-25 21:42:06 -0500

    Customize admin site

--- app/core/urls.py
+++ app/core/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+admin.site.index_title = "Site administration"
+admin.site.name = "Django Admin"
+admin.site.site_header = "Django administration"
+admin.site.site_title = "Django site admin"
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("accounts/", include("allauth.urls")),
