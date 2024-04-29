43d8ea171e480977e8570b471530ef8ec0191618
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-04-28 23:01:26 -0500

    Create error logging middleware

--- app/core/settings.py
+++ app/core/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
+    "common.middleware.ErrorLoggingMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
