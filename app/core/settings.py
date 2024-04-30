27231938fbc39e57d74fc26813bede1a32b1c03e
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-04-30 00:34:17 -0500

    Create prod.env

--- app/core/settings.py
+++ app/core/settings.py
@@ -14,6 +14,18 @@ DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
+CSRF_COOKIE_SECURE = env.bool("CSRF_COOKIE_SECURE", default=False)
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("SECURE_HSTS_INCLUDE_SUBDOMAINS", default=False)
+
+SECURE_HSTS_PRELOAD = env.bool("SECURE_HSTS_PRELOAD", default=False)
+
+SECURE_HSTS_SECONDS = env.int("SECURE_HSTS_SECONDS", default=0)
+
+SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=False)
+
+SESSION_COOKIE_SECURE = env.bool("SESSION_COOKIE_SECURE", default=False)
+
 DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
