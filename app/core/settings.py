13453484592539d60c9bd57c8d01a38296eed1b9
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-06-28 08:17:24 -0500

    Remove common app

--- app/core/settings.py
+++ app/core/settings.py
@@ -47,7 +47,6 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "common.apps.CommonConfig",
     "users.apps.UsersConfig",
 ]
 
@@ -63,7 +62,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
-    "common.middleware.ErrorLoggingMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
